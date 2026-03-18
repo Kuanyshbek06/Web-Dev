9d84d83e9df69df2384b0930be1e6751ac509c37
Kuanyshbek06 <kuanyshbek060609@gmail.com>
2026-03-18 11:10:35 +0500

    Updated version of task2

--- lab7/task2/models.py
+++ lab7/task2/models.py
@@ -1,32 +1,29 @@
 class Animal:
-    def __init__(self, name, age, color):
-        self.name = name
-        self.age = age
-        self.color = color
-
+    def __init__(self,name,age,color):
+        self.name=name
+        self.age=age
+        self.color=color
     def speak(self):
-        return "Animal makes a sound"
-
-    def info(self):
-        return f"{self.name} is {self.age} years old and {self.color}"
-
+        return "animal makes sound"
     def __str__(self):
-        return f"{self.name} ({self.age} years old, {self.color})"
-
-
+        return f"{self.name} is {self.age} years old and {self.color}"
+    
 class Dog(Animal):
-    def __init__(self, name, age, color, breed):
-        super().__init__(name, age, color)
-        self.breed = breed
-
+    def __init__(self,name,age,color,breed):
+        super().__init__(name,age,color)
+        self.breed=breed
+    
     def speak(self):
         return "Woof"
-
+    def __str__(self):
+        return super().__str__() + f", breed: {self.breed}"
 
 class Cat(Animal):
-    def __init__(self, name, age, color, lives):
-        super().__init__(name, age, color)
-        self.lives = lives
-
+    def __init__(self,name,age,color,lives):
+        super().__init__(name,age,color)
+        self.lives=lives
+    
     def speak(self):
         return "Meow"
+    def __str__(self):
+        return super().__str__()+ f",  lives: {self.lives}"
